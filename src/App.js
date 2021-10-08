77b1e205ec4765951dced36867577a75de58492e
Midnight <arreiskurai@yahoo.com>
2021-10-08 11:11:22 -0700

    Successfully made the search its own component

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ class App extends Component {
     super(props);
     this.state = { 
       songs: [],
-      searchedSongs: [],
-      searchType: 'artist',
-      searchTerm: '',
      }
   }
 
@@ -43,77 +40,16 @@ class App extends Component {
     });
   }
 
-  searchOnChange = (event) => {
+  resetSongs = (list) => {
     this.setState({ 
-      searchTerm : event.target.value 
-    })
-  }
-
-  searchSubmit = (event) => {
-    event.preventDefault();
-    this.searchSongs()
-  }
-
-  searchSongs = () => {
-    let searchType = this.state.searchType
-    let searchTerm = this.state.searchTerm.toLowerCase()
-    let newSongList = this.state.searchedSongs
-    this.state.songs.filter(function(foundSong){
-      debugger
-      switch(searchType){
-        case "artist":
-          if(foundSong.artist.toLowerCase() === searchTerm){
-            newSongList.push(foundSong)}
-          break;
-        case "album":
-          if(foundSong.album.toLowerCase() === searchTerm){
-            newSongList.push(foundSong)};
-          break;
-        case "genre":
-          if(foundSong.genre.toLowerCase().includes(searchTerm)){
-            newSongList.push(foundSong)};
-        break;
-        case "release_year":
-          if(foundSong.release_year === searchTerm){
-            newSongList.push(foundSong)};
-          break;
-        default:
-          break;
-      }
-    })
-    if(newSongList.length === 0){
-      alert('Your search yielded no results')
-    }
-    else{
-      this.setState({
-        songs : newSongList
-      })
-    }
-  }
-
-  submitFilterType = (event) => {
-    event.preventDefault();
-    this.setState({
-      searchType: event.target.elements.searchTypes.value
-    }, ()=> console.log(this.state.searchType))
+      songs : list
+     })
   }
 
   render() { 
     return(
       <div>
-        <form onSubmit = {this.submitFilterType}>
-          <select name="searchTypes">
-            <option value="artist">Artist</option>
-            <option value="title">Title</option>
-            <option value="genre">Genre</option>
-            <option value="release_year">Release Year</option>
-          </select>
-          <button type="submit" value="submit" >Lock In Search Type</button>
-        </form>
-        <form onSubmit={this.searchSubmit}>
-          <input type="text" placeholder="Type Search Here" onChange={this.searchOnChange}></input>
-          <button type="submit">Search</button>
-        </form>
+        <SearchForm songs={this.state.songs} resetSongs={this.resetSongs} />
         <DisplaySongs songs={this.state.songs} removeSong={this.removeSong} />
         <NewSong addNewSong={this.addNewSong} />
       </div>
---
+++ src/Components/SearchForm/SearchForm.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+class SearchForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            searchedSongs: [],
+            searchType: 'artist',
+            searchTerm: '',
+         }
+    }
+
+    searchOnChange = (event) => {
+        this.setState({ 
+          searchTerm : event.target.value 
+        })
+      }
+
+      searchSubmit = (event) => {
+        event.preventDefault();
+        this.searchSongs()
+      }
+
+      submitFilterType = (event) => {
+        event.preventDefault();
+        this.setState({
+          searchType: event.target.elements.searchTypes.value
+        }, ()=> console.log(this.state.searchType))
+      }
+      
+      searchSongs = () => {
+        let searchType = this.state.searchType
+        let searchTerm = this.state.searchTerm.toLowerCase()
+        let newSongList = this.state.searchedSongs
+        let songs = this.props.songs
+        songs.filter(function(foundSong){
+          switch(searchType){
+            case "artist":
+              if(foundSong.artist.toLowerCase() === searchTerm){
+                newSongList.push(foundSong)}
+              break;
+            case "title":
+            if(foundSong.title.toLowerCase() === searchTerm){
+                newSongList.push(foundSong)}
+            break;
+            case "album":
+              if(foundSong.album.toLowerCase() === searchTerm){
+                newSongList.push(foundSong)};
+              break;
+            case "genre":
+              if(foundSong.genre.toLowerCase().includes(searchTerm)){
+                newSongList.push(foundSong)};
+            break;
+            case "release_year":
+              if(foundSong.release_year === searchTerm){
+                newSongList.push(foundSong)};
+              break;
+            default:
+              break;
+          }
+        })
+        if(newSongList.length === 0){
+          alert('Your search yielded no results')
+        }
+        else{
+            let list = this.state.searchedSongs;
+            this.props.resetSongs(list)
+        }
+      }
+
+    render() { 
+        return ( 
+        <div>
+            <form onSubmit = {this.submitFilterType}>
+              <select name="searchTypes">
+                <option value="artist">Artist</option>
+                <option value="album">Album</option>
+                <option value="title">Title</option>
+                <option value="genre">Genre</option>
+                <option value="release_year">Release Year</option>
+              </select>
+              <button type="submit" value="submit" >Lock In Search Type</button>
+            </form>
+
+            <form onSubmit={this.searchSubmit}>
+              <input type="text" placeholder="Type Search Here" onChange={this.searchOnChange}></input>
+              <button type="submit">Search</button>
+            </form>
+        </div>
+        );
+    }
+}
+ 
+export default SearchForm;
